b061bd0c7d8ce3ef07a2481b9af78859efb0d710
Euan <psyec1@nottingham.ac.uk>
2018-04-05 14:17:54 +0100

    Weights init

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from torch.autograd import Variable
 import torch
 import torch.optim as optim
 import time
+import re
 
 #load video into a tensor
 filename = 'AFTERNOON.mp4'
 torch.backends.cudnn.benchmark = True
-print(torch.backends.cudnn.version())
 
 vidframes = preprocess.load_video(filename)
 temporalvolume = preprocess.bbc(vidframes)
@@ -18,7 +18,24 @@ temporalvolume = preprocess.bbc(vidframes)
 labels = Variable(torch.LongTensor([42, 42, 42, 42, 42, 42, 42, 42, 42, 42]).cuda())
 input = Variable(temporalvolume.cuda())
 
+#Create the model on the GPU.
 model = LipRead().cuda()
+
+#function to initialize the weights and biases of each module. Matches the
+#classname with a regular expression to determine the type of the module, then
+#initializes the weights for it.
+def weights_init(m):
+    classname = m.__class__.__name__
+    if re.search("Conv[123]d", classname):
+        m.weight.data.normal_(0.0, 0.02)
+    elif re.search("BatchNorm[123]d", classname):
+        m.weight.data.fill_(1.0)
+        m.bias.data.fill_(0)
+    elif re.search("Linear", classname):
+        m.bias.data.fill_(0)
+
+#Apply weight initialization to every module in the model.
+model.apply(weights_init)
 
 criterion = nn.NLLLoss().cuda()
 optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
@@ -28,7 +45,7 @@ startTime = time.time()
 def output_iteration(i, prediction, time):
     print("Iteration: {} \n Prediction: {} \n Time: {} \n".format(i, prediction, time))
 
-print("Starting training at {}...".format(startTime))
+print("Starting training...")
 for i in range(0,101):
     optimizer.zero_grad()
     outputs = model(input)
