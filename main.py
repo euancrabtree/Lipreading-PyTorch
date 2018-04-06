f6e3772a5e3088ca8281079362cff725357b2bdc
Euan <psyec1@nottingham.ac.uk>
2018-04-06 14:58:06 +0100

    Move weight init into model class

--- main.py
+++ main.py
@@ -16,22 +16,6 @@ torch.backends.cudnn.benchmark = True
 model = LipRead().cuda()
 #model = nn.DataParallel(model,device_ids=[0])
 
-#function to initialize the weights and biases of each module. Matches the
-#classname with a regular expression to determine the type of the module, then
-#initializes the weights for it.
-def weights_init(m):
-    classname = m.__class__.__name__
-    if re.search("Conv[123]d", classname):
-        m.weight.data.normal_(0.0, 0.02)
-    elif re.search("BatchNorm[123]d", classname):
-        m.weight.data.fill_(1.0)
-        m.bias.data.fill_(0)
-    elif re.search("Linear", classname):
-        m.bias.data.fill_(0)
-
-#Apply weight initialization to every module in the model.
-model.apply(weights_init)
-
 dataset = LipreadingDataset(0, 0)
 dataloader = DataLoader(dataset, batch_size=10,
                         shuffle=True, num_workers=10)
--- models/LipRead.py
+++ models/LipRead.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 
+import re
+
 from .ConvFrontend import ConvFrontend
 from .ResNetBBC import ResNetBBC
 from .LSTMBackend import LSTMBackend
@@ -15,6 +17,22 @@ class LipRead(nn.Module):
         self.resnet = ResNetBBC()
         self.lstm = LSTMBackend()
         self.convbackend = ConvBackend()
+
+        #function to initialize the weights and biases of each module. Matches the
+        #classname with a regular expression to determine the type of the module, then
+        #initializes the weights for it.
+        def weights_init(m):
+            classname = m.__class__.__name__
+            if re.search("Conv[123]d", classname):
+                m.weight.data.normal_(0.0, 0.02)
+            elif re.search("BatchNorm[123]d", classname):
+                m.weight.data.fill_(1.0)
+                m.bias.data.fill_(0)
+            elif re.search("Linear", classname):
+                m.bias.data.fill_(0)
+
+        #Apply weight initialization to every module in the model.
+        self.apply(weights_init)
 
     def forward(self, input):
         output = self.convbackend(self.resnet(self.frontend(input)))
